45faeababcaf5a2c4c5d162dd2aa231505090015
sonbm <sonbm@vingroup.vn>
2025-12-05 21:02:52 +0700

    update model and sign method

--- internal/auth/infra/persistence/user_model.go
+++ internal/auth/infra/persistence/user_model.go
@@ -6,10 +6,11 @@ import (
 
 type UserModel struct {
 	BaseModel
-	Name     string
-	Username string `gorm:"index:unique_user,unique"`
-	Email    string `gorm:"index:unique_user,unique"`
-	Password string `json:"-"`
+	Name       string
+	Username   string `gorm:"index:unique_user,unique"`
+	Email      string `gorm:"index:unique_user,unique"`
+	Password   string `json:"-"`
+	IsVerified bool
 }
 
 func (u *UserModel) TableName() string {
@@ -18,13 +19,17 @@ func (u *UserModel) TableName() string {
 
 func (u *UserModel) ToEntity() (*entity.User, error) {
 	return &entity.User{
-		ID:    u.ID,
-		Email: u.Email,
+		ID:         u.ID,
+		Email:      u.Email,
+		Password:   u.Password,
+		IsVerified: u.IsVerified,
 	}, nil
 }
 
 func (u *UserModel) FromEntity(e *entity.User) error {
 	u.ID = e.ID
 	u.Email = e.Email
+	u.Password = e.Password
+	u.IsVerified = e.IsVerified
 	return nil
 }
